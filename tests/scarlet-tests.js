0f0472db51d45047e3926ff815956572697a0ebb
tjchaplin <tjchaplin@hotmail.com>
2013-06-11 23:02:57 +0100

    Updated to fix issue with calling apply with the different "this" references

--- tests/scarlet-tests.js
+++ tests/scarlet-tests.js
@@ -7,32 +7,129 @@ function add1Interceptor(invocation) {
 	var result = invocation.proceed();
 	return result+1;
 };
-i = function(){
-
-};
-i.prototype.add2Interceptor = function (invocation) { 
-	var self = this;
-	console.log(invocation);
-	var result = invocation.proceed();
-	return result+1;
-};
 
 describe('Given an Interceptor Container',function(){
 
-		describe('When registering an interceptor',function(){
-			it("should call the interceptor",function(onComplete){
+		describe('When registering an interceptor for a method with a return to self',function(){
+			it("should not update self",function(onComplete){
 				Scarlet.resetAll();
-				function function1(){};
+				function function1(){
+					var self = this;
+					self.aValue = 1;
+				};
 				function1.prototype.return1 = function(){
-					return 1;
+					var self = this;
+					return self.aValue;
 				}
-				var j = new i();
-				Scarlet.register(j.add2Interceptor).forObject(function1);
+
+				Scarlet.register(add1Interceptor).forObject(function1);
 				var f1 = new function1();
 				var result = f1.return1();
 				result.should.be.eql(2);
 
 				onComplete();
+			});
+		});
+		describe('When registering an interceptor with a self reference',function(){
+			it("should not update self",function(onComplete){
+				Scarlet.resetAll();
+				var selfAddInterceptor = function(){
+					var self = this;
+					self.valueToAdd = 1;
+				};
+				selfAddInterceptor.prototype.add = function(invocation) { 
+					var self = this;
+					var result = invocation.proceed();
+					return result+self.valueToAdd;
+				};
+				function function1(){
+					var self = this;
+					self.aValue = 1;
+				};
+				function1.prototype.return1 = function(){
+					var self = this;
+					return self.aValue;
+				}
+
+				var interceptor = new selfAddInterceptor();
+				Scarlet.register(interceptor,interceptor.add).forObject(function1);
+				var f1 = new function1();
+				var result = f1.return1();
+				result.should.be.eql(2);
+
+				onComplete();
+			});
+		});
+		describe('When registering an interceptor that hasnt been initialized and a interceptor method has been specified',function(){
+			it("should throw error",function(onComplete){
+				try{
+					Scarlet.resetAll();
+					var selfAddInterceptor = function(){
+						var self = this;
+						self.valueToAdd = 1;
+					};
+					selfAddInterceptor.prototype.add = function(invocation) { 
+						var self = this;
+						var result = invocation.proceed();
+						return result+self.valueToAdd;
+					};
+
+					function function1(){};
+
+					Scarlet.register(selfAddInterceptor,selfAddInterceptor.add).forObject(function1);
+				}
+				catch(exception){
+
+					onComplete();
+				}
+				
+			});
+		});
+		describe('When registering an interceptor as an object with a self reference',function(){
+			it("should be able to intercept using a self reference",function(onComplete){
+				Scarlet.resetAll();
+				var selfAddInterceptor ={
+					valueToAdd : 1,
+					add : function(invocation) { 
+						var self = this;
+						var result = invocation.proceed();
+						return result+self.valueToAdd;
+					}
+				};
+
+				function function1(){
+					var self = this;
+					self.aValue = 1;
+				};
+				function1.prototype.return1 = function(){
+					var self = this;
+					return self.aValue;
+				}
+			
+				Scarlet.register(selfAddInterceptor,selfAddInterceptor.add).forObject(function1);
+				var f1 = new function1();
+				var result = f1.return1();
+				result.should.be.eql(2);
+
+				onComplete();
+			});
+		});
+		describe('When registering an interceptor as an object',function(){
+			describe('When no interceptor method specified in registration',function(){
+				it("should throw an error",function(onComplete){
+					try{
+						Scarlet.resetAll();
+						var selfUndefinedInterceptor ={};
+
+						function function1(){};
+					
+						Scarlet.register(selfAddInterceptor).forObject(function1);
+					}
+					catch(exception){
+
+						onComplete();
+					}
+				});
 			});
 		});
 		describe('When registering and then reseting the container',function(){

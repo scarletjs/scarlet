e088c87d778b44994921f756217e70be32c3407f
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2013-06-19 19:10:09 +0100

    implement grunt deploy task

--- gruntfile.js
+++ gruntfile.js
@@ -1,10 +1,12 @@
+
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     jshint: {
-      files: ['gruntfile.js', 'index.js', 'lib/**/*.js','test/**/*.js'],
+      files: ["gruntfile.js", "index.js", "lib/**/*.js","test/**/*.js"],
       options: {
         globals: {
           console: true,
@@ -15,7 +17,7 @@ module.exports = function(grunt) {
     release: {
       options: {
         bump: true, //default: true
-        file: 'package.json', //default: package.json
+        file: "package.json", //default: package.json
         add: true, 
         commit: true,
         tag: true,
@@ -25,14 +27,43 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      files: ["<%= jshint.files %>"],
+      tasks: ["jshint"]
     }
   });
 
-  grunt.loadNpmTasks('grunt-release');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-release");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask("default", ["jshint"]);
+  grunt.registerTask("deploy", ["release", "scarlet-bump"]);
+
+  grunt.registerTask("scarlet-bump", "A task for bumping release announcements to twitter", function(){
+
+    var done = this.async();
+    
+    var fs = require("fs");
+    require("string-format");
+    var http = require("http");
+    var prompt = require("prompt");
+
+    var project = "scarlet";
+    var package = fs.readFileSync("./package.json");
+    var version = JSON.parse(package).version;
+
+    console.log("Please enter the scarlet twitter password to bump the release annnouncement:");
+
+    prompt.start();
+
+    prompt.get(["password"], function(err, result){
+      var req = http.get("http://www.scarletjs.com/release/bump?project={0}&version={1}&auth={2}".format(project, version, result.password), function(res) {
+        console.log("Scarletjs.com: " + res.statusCode);
+        console.log("Scarletjs.com: " + JSON.stringify(res.headers));
+        done();
+      });
+
+    });
+
+  });
 };

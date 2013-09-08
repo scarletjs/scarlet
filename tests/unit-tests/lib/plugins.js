d7583749881e875167c2fcc80bbe928460e1e632
fir3pho3nixx <fir3pho3nixx@gmail.com>
2013-09-08 23:23:56 +0100

    Added some unit tests for the plugin object

---
+++ tests/unit-tests/lib/plugins.js
@@ -0,0 +1,33 @@
+require("../../../include");
+
+
+module.exports = function($scarlet){
+
+	var self = this;
+	self.initializeWasCalled = false;
+
+	self.initialize = function(){
+		self.initializeWasCalled = true;
+	};
+
+};
+
+describe("Given /lib/Plugins", function(){
+
+	var Scarlet = require("../../../lib/scarlet");
+	var pluginManager = require("../../../lib/plugins");
+
+	describe("When #loadPlugin()", function(){
+
+		var $scarlet = new Scarlet();
+
+		pluginManager.setPluginPath(__dirname + "/");
+
+		it("Then should load and call initialize", function(){
+			var plugin = pluginManager.loadPlugin($scarlet, "plugins");
+			assert(plugin.initializeWasCalled);
+		});
+
+	});
+
+});

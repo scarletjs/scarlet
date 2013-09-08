128b8b28d821e627cf7f21fd3d313c8786b897aa
fir3pho3nixx <fir3pho3nixx@gmail.com>
2013-09-08 14:22:44 +0100

    Tested the hasMember in object extensions

--- tests/unit-tests/lib/extensions/object.js
+++ tests/unit-tests/lib/extensions/object.js
@@ -19,13 +19,56 @@ describe("Given lib/extensions/Object", function(){
 	describe("When #isNull()", function(){
 
 		var nullObj = null;
+		var undefObj = undefined;
 
 		it("Then should return true for null reference", function(){
 			assert(ext.object.isNull(nullObj));
 		});
 
+		it("Then should return true for undefined", function(){
+			assert(ext.object.isNull(undefObj));
+		});
+
 		it("Then should return false for non null reference", function(){
 			assert(!ext.object.isNull(new function(){}));
+		});
+
+	});
+
+	describe("When #hasMember() for classes", function(){
+
+		var anyClass = function(){
+			var self = this;
+			self.anyProperty = "Foo";
+			self.anyMethod = function(){};
+		};
+
+		it("Then it should be able to find a method", function(){
+			assert(ext.object.hasMember(anyClass, "anyMethod"));
+		});
+
+		it("Then it should be able to find a property", function(){
+			assert(ext.object.hasMember(anyClass, "anyProperty"));
+		});
+
+	});
+
+	describe("When #hasMember for instances", function(){
+
+		var anyClass = function(){
+			var self = this;
+			self.anyProperty = "Foo";
+			self.anyMethod = function(){};
+		};
+
+		var anyInstance = new anyClass();
+
+		it("Then it should be able to find a method", function(){
+			assert(ext.object.hasMember(anyInstance, "anyMethod"));
+		});
+
+		it("Then it should be able to find a property", function(){
+			assert(ext.object.hasMember(anyInstance, "anyProperty"));
 		});
 
 	});

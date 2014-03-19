b358014e6b410170f84c08f112e1846f8a1fc9e3
fir3pho3nixx <fir3pho3nixx@gmail.com>
2014-03-19 22:36:04 +0000

    More updates to content

--- public/javascripts/interpreter/shell.js
+++ public/javascripts/interpreter/shell.js
@@ -11,6 +11,13 @@ define("interpreter/shell", ["lodash", "scarlet"], function(_, Scarlet) {
 				__outputBuffer += "<span style='color: #3C403B'># " + new Date().format("hh:mm:ss") + ":</span>&gt; " + str + "<br/>\n";
 			}
 
+			var traceInterceptor = function(info, method, args){
+			  var result = method.call(this, info, method, args);
+			  var query = scarlet.interceptQuery(info, method, args, result);
+			  query.traceTo(print);
+			  return result;
+			};
+
 			try
 			{
 				eval(command);

694e3a17bfea05a33ee1bfb729be9e124c3baa86
tjchaplin <tjchaplin@hotmail.com>
2014-04-18 09:14:09 +0100

    updated to include gulp release task to bump tag and push

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,74 @@
 var gulp = require("gulp");
+var git = require("gulp-git");
 var mox = require("gulp-mox");
 var bump = require("gulp-bump");
 var gutil = require("gulp-util");
 var mocha = require("gulp-mocha");
 var jshint = require("gulp-jshint");
-var rename = require('gulp-rename');
+var rename = require("gulp-rename");
 var browserify = require("gulp-browserify");
 
 gulp.task("default", ["lint","test"]);
 
-gulp.task("release",["lint","test","browserify","documentation","bump"]);
-
-gulp.task("documentation",function(){
-    gulp.src(["lib/scarlet.js","lib/interceptors/**/*.js"])
-        .pipe(mox())
-        .pipe(gulp.dest("./docs/markdown/"));
-});
-
-gulp.task("browserify", function() {
-    gulp.src("./index.js", { read: false} )
-        .pipe(browserify({
-            standalone: "scarlet"
-        }))
-        .pipe(gulp.dest("./pub/scarlet.js/"))
-});
-
-gulp.task("bump", function(){
-  gulp.src("./package.json")
-  .pipe(bump())
-  .pipe(gulp.dest("./"));
-});
+gulp.task("release",["lint","test","browserify","documentation","tag"]);
 
 gulp.task("watch", function() {
     gulp.watch(["lib/**/*.js","tests/**/*.js"], ["catchErrorTest"]);
 });
 
 gulp.task("lint", function() {
-  gulp.src(["./lib/**/*.js",
+  return gulp.src(["./lib/**/*.js",
             "./tests/**/*.js"])
-    .pipe(jshint())
-    .pipe(jshint.reporter("default"));
+            .pipe(jshint())
+            .pipe(jshint.reporter("default"));
 });
 
 gulp.task("catchErrorTest",function(){
-    gulp.src(["tests/**/*.js","!/tests/spec/builders/**/*.js"])
-        .pipe(mocha({ reporter: "list" }))
-        .on("error", gutil.log);
+    return gulp.src(["tests/**/*.js","!/tests/spec/builders/**/*.js"])
+                .pipe(mocha({ reporter: "list" }))
+                .on("error", gutil.log);
 });
 
 gulp.task("test",["unitTest","bddTest"]);
 
 gulp.task("unitTest",function(){
-    gulp.src(["tests/unit/**/*.js"])
-        .pipe(mocha({ reporter: "spec" }));
+    return gulp.src(["tests/unit/**/*.js"])
+                .pipe(mocha({ reporter: "spec" }));
 });
 
 gulp.task("bddTest",function(){
-    gulp.src(["tests/spec/**/*.js","!/tests/spec/builders/**/*.js"])
-        .pipe(mocha({ reporter: "spec" }));
-})
+    return gulp.src(["tests/spec/**/*.js","!/tests/spec/builders/**/*.js"])
+                .pipe(mocha({ reporter: "spec" }));
+});
+
+gulp.task("documentation",function(){
+    return gulp.src(["lib/scarlet.js","lib/interceptors/**/*.js"])
+                .pipe(mox())
+                .pipe(gulp.dest("./docs/markdown/"));
+});
+
+gulp.task("browserify", function() {
+    return gulp.src("./index.js", { read: false} )
+                .pipe(browserify({
+                    standalone: "scarlet"
+                }))
+                .pipe(gulp.dest("./pub/scarlet.js/"))
+});
+
+gulp.task("bump", function(){
+  return gulp.src("./package.json")
+              .pipe(bump())
+              .pipe(gulp.dest("./"));
+});
+
+
+gulp.task("tag",["bump"] ,function () {
+  var version = require("./package.json").version;
+  gutil.log('Tagging:'+version);
+  
+  return gulp.src('./')
+            .pipe(git.commit(version))
+            .pipe(git.tag(version, version))
+            .pipe(git.push('origin', 'master', '--tags'))
+            .pipe(gulp.dest('./'));
+});
